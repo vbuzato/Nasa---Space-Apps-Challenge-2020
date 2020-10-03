befef4a083a6c85da4394c375cfc699f8e703766
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-10-03 18:25:59 -0300

    Geocode API working well

--- geoapi copy.js
+++ geoapi copy.js
@@ -1,12 +1,13 @@
+const axios = require('axios');
+
 function fetchData(city) {
   const params = {
-    method: "POST",
     auth: '31727618108010456649x17679',
-    locate: 'Curitiba',
+    locate: city,
     json: '1'
   }
   
-  fetch('https://geocode.xyz', {params})
+  axios.get('https://geocode.xyz', {params})
     .then(response => {
       // console.log(`Latitude: ${response.data.latt}`);
       // console.log(`Longitude: ${response.data.longt}`);
@@ -16,31 +17,6 @@ function fetchData(city) {
       console.log(error);
     });
 }
-
-
-
-
-
-
-// const axios = require('axios');
-
-// function fetchData(city) {
-//   const params = {
-//     auth: '31727618108010456649x17679',
-//     locate: city,
-//     json: '1'
-//   }
-  
-//   axios.get('https://geocode.xyz', {params})
-//     .then(response => {
-//       // console.log(`Latitude: ${response.data.latt}`);
-//       // console.log(`Longitude: ${response.data.longt}`);
-//       console.log(`Tudo: ${response.data}`);
-//       //return response.data;
-//     }).catch(error => {
-//       console.log(error);
-//     });
-// }
 
 //fetchData('Curitiba')
 
--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-import { fetchData } from './geoapi';
+let latt;
+let longt;
 
-const latitude = '-29.3458742';
-const longitude = '-15.6940431';
-const baseURL = `https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d14726403.993844409!2d${latitude}!3d${longitude}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1601728945839!5m2!1spt-BR!2sbr`
+async function fetchData(address) {
+  const editedAddress = address.replace(' ', '+');
+  const params = {
+    auth: '31727618108010456649x17679',
+    locate: address,
+    json: '1'
+  }
+  
+  const fetchGeocode =  await fetch(`https://geocode.xyz/Hauptstr.,+${editedAddress}?json=1`, { params });
+  const response = await fetchGeocode.json();
+  console.log(`Latitude: ${response.latt}`);
+  console.log(`Longitude: ${response.longt}`);
+  console.log(`Tudo: ${JSON.stringify(response)}`);
+}
 
 const btFind = document.querySelector('.bt-find');
 btFind.addEventListener('click', async () => {
-  const city = document.querySelector('#input-city').value;
-  const result = fetchData(city);
-  console.log(result);
-  // const map = document.querySelector('iframe');
-  // map.src = baseURL;
+  
+  const address = document.querySelector('#input-city').value;
+  fetchData(address);
+
+  const baseURL = `https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d14726403.993844409!2d${latt}!3d${longt}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1601728945839!5m2!1spt-BR!2sbr`
+  const map = document.querySelector('iframe');
+  map.src = baseURL;
 });
 
 //  setTimeout(() => {
