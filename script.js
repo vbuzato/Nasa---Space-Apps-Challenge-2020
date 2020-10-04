6409f8d7daef2122131b32480e3faf4dc90db0b5
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-10-04 13:12:49 -0300

    Busca da localização implementada

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
-let latt = 0;
-let longt = 0;
-let init = 0;
-
 async function fetchData(address) {
-  const editedAddress = address.replace(' ', '+');
+  const editedAddress = (address) ? address.replace(' ', '+') : null;
   const params = {
     auth: '31727618108010456649x17679',
     locate: address,
     json: '1'
   }
-  
   const fetchGeocode =  await fetch(`https://geocode.xyz/Hauptstr.,+${editedAddress}?json=1`, { params });
   const response = await fetchGeocode.json();
-  latt = response.latt;
-  longt = response.longt;
-  // console.log(`Latitude: ${response.latt}`);
-  // console.log(`Longitude: ${response.longt}`);
-  console.log(`Tudo: ${JSON.stringify(response)}`);
-  //const map = L.map('map').panTo([latt, longt], 3);
-  renderMap()
+  let latt = response.latt;
+  let longt = response.longt;
+  return { latt, longt };
 }
-function updateMapState() {
-  const address = document.querySelector('#input-address').value;
-  fetchData(address);
+async function updateMapState() {
+  const address = await document.querySelector('#input-address').value;
+  await renderMap(address)
 }
 
 const btFind = document.querySelector('.bt-find');
@@ -30,21 +21,23 @@ btFind.addEventListener('click', () => updateMapState());
 const inputFind = document.querySelector('#input-address');
 inputFind.addEventListener('keydown', (e) => { if (e.keyCode === 13) updateMapState() });
 
-function renderMap() {
-  const map = L.map('map');
-  (init === 0) ? map.setView([latt, longt], 3) : map.viewreset([latt, longt], 3);
-  init += 1;
-  let marker;
-  if (latt !== 0 && longt !== 0) {
-    //const mapContent = document.querySelector('#map').innerHTML = '';
-    marker = L.marker([latt, longt]).addTo(map);
+async function renderMap(address) {
+  document.getElementById('weathermap').innerHTML = "<div id='map' class='map'></div>";
+  let { latt, longt } = await fetchData(address);
+  const map = await L.map('map');
+  const lattNumber = parseFloat(latt);
+  const longtNumber = parseFloat(longt);
+  console.log(latt, longt);
+  if (lattNumber !== 26.22510 && longtNumber !== -98.17523) {
+    map.setView([lattNumber, longtNumber], 5);
   }
-
-  // https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=29nSLs65L7utfzxqP33a
-  // https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=29nSLs65L7utfzxqP33a
+  L.marker([lattNumber, longtNumber]).addTo(map);
+  map.setView([lattNumber, longtNumber], 3);
 
   L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=29nSLs65L7utfzxqP33a', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
   }).addTo(map);
 }
-renderMap();
+window.onload = () => {
+  renderMap();
+}
