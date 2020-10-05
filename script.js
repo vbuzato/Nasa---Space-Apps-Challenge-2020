afe536b9c9a6af67483f006f23d34e2e036f3d39
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-10-05 00:38:22 -0300

    Projeto finalizado de acordo com a prospota da equipe e possibilidades de requisições na api da NASA.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// __________Requisições NASA__________
+async function fetchSatellites() {
+  const url = `https://data.ivanstanojevic.me/api/tle?page=20`
+  const fetchSat = await fetch(`${url}`)
+  const response = await fetchSat.json()
+  const satellite = response.member
+  const nameId = satellite.map(each => each.line2.slice(2, 7))
+  const latLongt = satellite.map(each => each.line2.slice(9, 25))
+  const latitude = latLongt.map(each => each.slice(0, 7))
+  const longitude = latLongt.map(each => {
+    const long = each.slice(8, 16);
+    if(long > 180) {
+      let longtNumber = long - 360
+      return longtNumber.toFixed(7);
+    }
+    return long;
+  })
+  let arraySat = [] 
+  for(let i = 0; i < nameId.length; i++) {
+    arraySat.push({ name:nameId[i], latt: parseFloat(latitude[i]), longt: parseFloat(longitude[i]) })
+  }
+  return arraySat;
+}
+
 // __________Requisição da Localização__________
 async function fetchData(address) {
   const editedAddress = (address) ? address.replace(' ', '+') : null;
@@ -16,10 +40,10 @@ async function fetchData(address) {
 
 // __________Rederização dos Satellites__________
 
-function satAround(latt, longt) {
-  const rangeLatt = 20.00;
-  const rangeLongt = 50.00;
-  const satellites = [{ name: 'name01', latt: -35.2227398, longt: -39.2302285 }, { name: 'name02', latt: 15.2227398, longt: 129.2302285 }, { name: 'name03', latt: -15.7727398, longt: -30.8302285 }];
+async function satAround(latt, longt) {
+  const rangeLatt = 50.00;
+  const rangeLongt = 100.00;
+  const satellites = await fetchSatellites();
 
   const satellitesFilter = satellites.filter((satellite) => {
     return satellite.latt < (latt + rangeLatt)
@@ -37,25 +61,15 @@ async function renderMap(address) {
   const map = await L.map('map');
   const lattNumber = parseFloat(latt);
   const longtNumber = parseFloat(longt);
-  //console.log(latt, longt);
-
-  // if (lattNumber !== 26.22510 && longtNumber !== -98.17523) {
-  //   map.setView([lattNumber, longtNumber], 5);
-  // }
-
-  const arrSatellites = satAround(lattNumber, longtNumber);
+  const arrSatellites = await satAround(lattNumber, longtNumber);
   console.log(arrSatellites);
   arrSatellites.forEach((sat) => {
     console.log(sat);
-    //L.marker([sat.latt, sat.longt]).addTo(map);
-
     var imageUrl = 'images/satellite.svg',
     imageBounds = [[sat.latt, sat.longt], [sat.latt + 5, sat.longt + 5]];
     L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
-
   })
-
+  L.marker([74.0044, -64.6653]).addTo(map);
   L.marker([lattNumber, longtNumber]).addTo(map);
   map.setView([lattNumber, longtNumber], 4);
 
@@ -63,8 +77,6 @@ async function renderMap(address) {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
   }).addTo(map);
 }
-
-
 
 // __________Variáveis e Eventos__________
 async function updateMapState() {
